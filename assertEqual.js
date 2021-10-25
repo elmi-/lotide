2868bb99278e8de6c77f93ece41591d0a93a5fd7
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-10-25 23:57:37 +0000

    improved assertEqual.js with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
 // Implement assertEqual
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    // console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
   } else {
-    console.log('❌ Assertion Falied: ' + actual + ' !== ' + expected);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 

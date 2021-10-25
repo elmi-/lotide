6007411e2e47e94d3bba2bd9926aa0bf0f00554c
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-10-25 23:59:43 +0000

    improved assertEqual.js with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// Implement assertEqual
+// Implement assertEqual w/ template literals
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);

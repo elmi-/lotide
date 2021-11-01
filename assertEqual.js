479357ab50ad6c7887be8569b6b878a774604e0a
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-01 23:59:13 +0000

    refactored lotide head.js assertEqual.js by modularizing code into separate files

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1.1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    // console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   if (array && array.length > 0) {
@@ -13,8 +6,4 @@ const head = function(array) {
   }
 };
 
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([]), undefined);
+module.exports = head;

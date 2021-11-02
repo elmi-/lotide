8f659c5ab9869ff696368d45f6891a3fccce8bf1
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-02 00:14:10 +0000

    refactored lotide assertArraysEqual.js by modularizing code into separate files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let outcome;
-  if (arr1.length === arr2.length) {
-    let arrayLength = arr2.length;
-    for (let i = 0; i < arrayLength; i++) {
-      if (arr1[i] === arr2[i]) {
-        outcome = true;
-      } else {
-        outcome = false;
-      }
-    }
-  } else {
-    outcome = false;
-  }
-  return outcome;
-};
-
-/*
-Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
-*/
-
 const assertArraysEqual = function(outcome) {
   if (outcome) {
     console.log(`✅ Assertion Passed: eqArrays function returns ${outcome}`);
@@ -27,6 +6,4 @@ const assertArraysEqual = function(outcome) {
   }
 };
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2])); // => false
+module.exports = assertArraysEqual;
